ba22dc2193b0b3a032adb9b605c983337a29bf96
joseph241006 <gualice939@gmail.com>
2024-12-02 16:26:58 +0700

    Update kodo_test.go

--- s3/kodo/kodo_test.go
+++ s3/kodo/kodo_test.go
@@ -1,6 +1,7 @@
 package kodo_test
 
 import (
+	"context"
 	. "github.com/ammmnia/tools/s3/kodo"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -9,12 +10,16 @@ import (
 func TestNewKodo(t *testing.T) {
 	k, err := NewKodo(Config{
 		Endpoint:        "https://flyim.s3.ap-northeast-2.amazonaws.com",
-		Bucket:          "flyim",
+		Bucket:          "ap-northeast-2",
 		BucketURL:       "https://flyim.s3.ap-northeast-2.amazonaws.com",
 		AccessKeyID:     "AKIAW3MEABHC2EOP5RD2",
 		AccessKeySecret: "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",
 	})
 	assert.Nil(t, err)
 	assert.Equal(t, "kodo", k.Engine())
-
+	upload, err := k.InitiateMultipartUpload(context.TODO(), "xhxh")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, "xhxh", upload.UploadID)
 }
