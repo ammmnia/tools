fa541084292a1a45aab4ce7d3c4d6485c51e7193
joseph241006 <gualice939@gmail.com>
2024-12-16 21:01:40 +0700

    Update constant_meta.go

--- constant/constant_meta.go
+++ constant/constant_meta.go
@@ -1,16 +1,16 @@
 package constant
 
-type MetaKey string
+type MetaKey = string
 
 const (
-	OperationID     MetaKey = "operationID"
-	OpUserID        MetaKey = "opUserID"
-	ConnID          MetaKey = "connID"
-	OpUserPlatform  MetaKey = "platform"
-	Token           MetaKey = "token"
-	RpcCustomHeader MetaKey = "customHeader" // rpc中间件自定义ctx参数
-	CheckKey        MetaKey = "CheckKey"
-	TriggerID       MetaKey = "triggerID"
-	RemoteAddr      MetaKey = "remoteAddr"
-	Account         MetaKey = "account"
+	OperationID     = "operationID"
+	OpUserID        = "opUserID"
+	ConnID          = "connID"
+	OpUserPlatform  = "platform"
+	Token           = "token"
+	RpcCustomHeader = "customHeader" // rpc中间件自定义ctx参数
+	CheckKey        = "CheckKey"
+	TriggerID       = "triggerID"
+	RemoteAddr      = "remoteAddr"
+	Account         = "account"
 )
