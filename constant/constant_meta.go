7e12bfca8c5c6b1f4cf49f235c92b9e325b1cbcf
elvin <elvin@gmail.com>
2025-01-17 21:20:19 +0700

    1

--- constant/constant_meta.go
+++ constant/constant_meta.go
@@ -3,13 +3,14 @@ package constant
 type MetaKey = string
 
 const (
-	OperationID     = "operationID"
-	OpUserID        = "opUserID"
-	ConnID          = "connID"
-	OpUserPlatform  = "platform"
-	Token           = "token"
-	RpcCustomHeader = "customHeader" // rpc中间件自定义ctx参数
-	CheckKey        = "CheckKey"
-	TriggerID       = "triggerID"
-	RemoteAddr      = "remoteAddr"
+	OperationID      = "operationID"
+	OpUserID         = "opUserID"
+	ConnID           = "connID"
+	OpUserPlatform   = "platform"
+	Token            = "token"
+	RpcCustomHeader  = "customHeader" // rpc中间件自定义ctx参数
+	CheckKey         = "CheckKey"
+	TriggerID        = "triggerID"
+	RemoteAddr       = "remoteAddr"
+	SuperAdminUserId = "superAdminUserId"
 )
